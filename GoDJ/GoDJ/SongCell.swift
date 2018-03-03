7fbd6a5721efda0049d8ac5e40bb541f0afa02f4
ALYSSA <alyssasgable@gmail.com>
2018-03-03 09:38:45 -0500

    Added zach's code, fixed all erors

---
+++ GoDJ/GoDJ/SongCell.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class SongCell: UITableViewCell {
+    
+    
+    var songLabel: UILabel = {
+        $0.textColor = UIColor.black
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+        $0.lineBreakMode = .byWordWrapping
+        $0.sizeToFit()
+        return $0
+    }(UILabel())
+
+	var imageItem = UIImageView()
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+		self.contentView.addSubview(songLabel)
+		songLabel.snp.makeConstraints { (make) in
+			make.edges.equalToSuperview()
+		}
+		imageItem.image = UIImage(named: "")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ GoDJ/GoDJ/SongQueue.swift
@@ -0,0 +1,52 @@
+//
+//  DJHomeVC.swift
+//  GoDJ
+//
+//  Created by ALYSSA on 3/3/1var//  Copyright © 2018 AlyssaGable. All rights reserved.
+//
+
+import UIKit
+
+let cellReuseIdentifier = "TableCellIdentifier"
+
+
+
+class SongQueue: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableView = UITableView()
+    
+    var feedArray = [String]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    var djName = ""
+    
+    init(djNameIn: String) {
+        djName = djNameIn
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return feedArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! SongCell
+        cell.songLabel = feedArray[indexPath.row]
+        return cell
+    }
+ 
+}
